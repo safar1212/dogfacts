8282b8e9646f3428d749d713c7feaa9c29408655
safar ali <miqra019@gmail.com>
2022-07-29 21:02:02 +0500

    Resolve linters errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './Components/HomePage';
-import NavBar from './Components/Navbar';
+import NavBar from './Components/Navbar.jsx';
 import Details from './Components/Details';
 
 function App() {
--- src/tests/HomePage.test.js
+++ src/tests/HomePage.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from '../redux/configureStore';
+import store from '../redux/configureStore.jsx';
 import HomePage from '../Components/HomePage';
 
 describe('Details test', () => {
